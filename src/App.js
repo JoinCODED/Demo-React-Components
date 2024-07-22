a01f6883d8b14569c191ddbe753a243d814b54ea
SalwaBaqer <s.abaqer@hotmail.com>
2024-07-22 16:40:58 +0300

    add starter code

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import "./App.css";
 import cookies from "./cookies";
 
 function App() {
+  const cookiesList = cookies.map((cookie) => (
+    <div key={cookie.id} className="cookie-item">
+      <img src={cookie.image} className="cookie-image" />
+      <p>{cookie.name}</p>
+      <p>{cookie.price}</p>
+    </div>
+  ));
   return (
     <div className="App App-header">
       <h1>Cookies</h1>
+      <div className="cookie-list">{cookiesList}</div>
     </div>
   );
 }
