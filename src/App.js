03492c3959d5c7307212788e97d3e6634b588682
SalwaBaqer <s.abaqer@hotmail.com>
2024-07-22 16:36:03 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import cookies from "./cookies";
+
+function App() {
+  return (
+    <div className="App App-header">
+      <h1>Cookies</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/cookies.js
@@ -0,0 +1,25 @@
+const cookies = [
+  {
+    id: 1,
+    name: "Milk chocolate",
+    image:
+      "https://benscookies.ph/wp-content/uploads/2017/06/product-peanut-and-milk-choc-324x324.png",
+    price: "1,000",
+  },
+  {
+    id: 2,
+    name: "Double Chocolate Nuts",
+    image:
+      "https://benscookies.ph/wp-content/uploads/2017/06/product-double-nut-324x324.png",
+    price: "1,500",
+  },
+  {
+    id: 3,
+    name: "White Chocolate Macadamia",
+    image:
+      "https://benscookies.ph/wp-content/uploads/2017/06/product-macadamia-and-white-choc.png",
+    price: "1,800",
+  },
+];
+
+export default cookies;
